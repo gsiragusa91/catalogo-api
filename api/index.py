c747132de8e000f6b0e9f73f7f32e9223b990e18
gsiragusa91 <guido.siragusa@gmail.com>
2025-05-03 14:09:55 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://catalogo-frontend-iota.vercel.app"]
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
